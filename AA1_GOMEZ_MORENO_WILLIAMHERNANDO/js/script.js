b65796f4b40b953de1f463d95caee23d7f90cf5d
William Gomez <you@example.comwhgomez@ucompensar.edu.co>
2024-05-15 21:48:55 -0500

    Ajuste Modal Index

--- AA1_GOMEZ_MORENO_WILLIAMHERNANDO/js/script.js
+++ AA1_GOMEZ_MORENO_WILLIAMHERNANDO/js/script.js
@@ -1,20 +1,19 @@
-$(document).ready(function() {
-    $('#search-form').submit(function(event) {
-        event.preventDefault(); // Evitar que el formulario se envíe
+// script.js
 
-        var searchTerm = $('#search-input').val().toLowerCase(); // Obtener el término de búsqueda en minúsculas
+// Función para buscar una película
+function buscarPelicula() {
+    const busquedaInput = document.getElementById('busquedaInput').value.toLowerCase();
+    const peliculas = document.getElementsByClassName('pelicula');
 
-        // Filtrar las tarjetas de películas según el término de búsqueda
-        $('.card').each(function() {
-            var title = $(this).find('.card-title').text().toLowerCase(); // Obtener el título de la película en minúsculas
-            var director = $(this).find('.director').text().toLowerCase(); // Obtener el nombre del director en minúsculas
+    for (let pelicula of peliculas) {
+        const titulo = pelicula.getElementsByClassName('titulo')[0].textContent.toLowerCase();
+        if (titulo.includes(busquedaInput)) {
+            pelicula.style.display = '';
+        } else {
+            pelicula.style.display = 'none';
+        }
+    }
+}
 
-            // Mostrar u ocultar la tarjeta según el criterio de búsqueda
-            if (title.includes(searchTerm) || director.includes(searchTerm)) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    });
-});
+// Event listener para el campo de búsqueda
+document.getElementById('busquedaInput').addEventListener('input', buscarPelicula);
